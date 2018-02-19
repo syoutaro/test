34dae0b8c73e9d34412f8065d1639cee262e97d3
sugawara <pgjsugawara258@icloud.com>
2018-02-19 17:50:52 +0900

    0219

---
+++ 4each_keijiban/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+<meta charset="utf-8">
+<title>4eachblog 掲示板</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+
+</head>
+
+<body>
+ <header>
+  <img src="4eachblog_logo.jpg">   
+    
+ </header>    
+    
+</body>
+
+</html>
